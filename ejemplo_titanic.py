10a3898cdcd7009440745d8ee2922e3c48e6fca1
joseignaciosbm-rgb <joseignaciosbm@gmail.com>
2025-11-12 21:27:28 -0300

    Add import for os module

--- ejemplo_titanic.py
+++ ejemplo_titanic.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 # Carga el archivo CSV "database_titanic.csv" en un DataFrame de pandas.
 df = pd.read_csv("database_titanic.csv")
